f7ce03dc90cd16aa2926d0277845335c5a8689d5
Marc J. Schmidt <marc@kryn.org>
2014-09-08 00:09:43 +0200

    Removed that aggressive license stuff.

--- src/ResizeSensor.js
+++ src/ResizeSensor.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Marc J. Schmidt. See the LICENSE file at the top-level
+ * Copyright Marc J. Schmidt. See the LICENSE file at the top-level
  * directory of this distribution and at
  * https://github.com/marcj/css-element-queries/blob/master/LICENSE.
  */
