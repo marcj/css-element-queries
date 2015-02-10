3f87e2a4dd0eeb79a2583a97d42bd6d8bb7d2219
Marc J. Schmidt <marc@marcjschmidt.de>
2015-02-10 17:33:44 +0100

    Added ondomready inits

--- src/ElementQueries.js
+++ src/ElementQueries.js
@@ -288,15 +288,47 @@
 
     ElementQueries.withTracking = false;
 
-    function init() {
-        ElementQueries.instance = new ElementQueries();
+    ElementQueries.init = function() {
+        if (!ElementQueries.instance) {
+            ElementQueries.instance = new ElementQueries();
+        }
+
         ElementQueries.instance.init(ElementQueries.withTracking);
-    }
+    };
+
+    var domLoaded = function (callback) {
+        /* Internet Explorer */
+        /*@cc_on
+        @if (@_win32 || @_win64)
+            document.write('<script id="ieScriptLoad" defer src="//:"><\/script>');
+            document.getElementById('ieScriptLoad').onreadystatechange = function() {
+                if (this.readyState == 'complete') {
+                    callback();
+                }
+            };
+        @end @*/
+        /* Mozilla, Chrome, Opera */
+        if (document.addEventListener) {
+            document.addEventListener('DOMContentLoaded', callback, false);
+        }
+        /* Safari, iCab, Konqueror */
+        if (/KHTML|WebKit|iCab/i.test(navigator.userAgent)) {
+            var DOMLoadTimer = setInterval(function () {
+                if (/loaded|complete/i.test(document.readyState)) {
+                    callback();
+                    clearInterval(DOMLoadTimer);
+                }
+            }, 10);
+        }
+        /* Other web browsers */
+        window.onload = callback;
+    };
 
     if (window.addEventListener) {
-        window.addEventListener('load', init, false);
+        window.addEventListener('load', ElementQueries.init, false);
     } else {
-        window.attachEvent('onload', init);
+        window.attachEvent('onload', ElementQueries.init);
     }
+    domLoaded(ElementQueries.init);
 
 })();
