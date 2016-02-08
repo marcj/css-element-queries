89bf00532bf5d58e60c268f083f41465dbec902d
Alberto Villa <avilla@FreeBSD.org>
2016-02-08 03:08:40 +0100

    Fix selectors by injecting an uncommon element
    Injecting a <div> can break selectors like :last-of-type. We can
    use something more rare (with no layout value) like <area> to make the
    sensor more transparent to JavaScript and CSS.

--- src/ResizeSensor.js
+++ src/ResizeSensor.js
@@ -69,9 +69,9 @@
                 return;
             }
 
-            element.resizeSensor = document.createElement('div');
+            element.resizeSensor = document.createElement('area');
             element.resizeSensor.className = 'resize-sensor';
-            var style = 'position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: hidden; z-index: -1; visibility: hidden;';
+            var style = 'display: block; position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: hidden; z-index: -1; visibility: hidden;';
             var styleChild = 'position: absolute; left: 0; top: 0; transition: 0s;';
 
             element.resizeSensor.style.cssText = style;
