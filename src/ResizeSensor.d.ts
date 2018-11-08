1631dea1f96614d2defc895d5020a4210981c9f0
mikcol <Michael.Coleman@sas.com>
2018-11-08 10:06:42 -0500

    renaming ResizeCallback to ResizeSensorCallback

--- src/ResizeSensor.d.ts
+++ src/ResizeSensor.d.ts
@@ -1,11 +1,11 @@
-export declare type ResizeCallback = (size: { width: number; height: number; }) => void;
+export declare type ResizeSensorCallback = (size: { width: number; height: number; }) => void;
 
 declare class ResizeSensor {
-    constructor(element: Element | Element[], callback: ResizeCallback);
-    detach(callback: ResizeCallback): void;
+    constructor(element: Element | Element[], callback: ResizeSensorCallback);
+    detach(callback: ResizeSensorCallback): void;
     reset(): void;
 
-    static detach(element: Element | Element[], callback: ResizeCallback): void;
+    static detach(element: Element | Element[], callback: ResizeSensorCallback): void;
     static reset(element: Element | Element[]): void;
 }
 
