5eda8e751c465208393a1a730ac3ebe1b2e27b08
mikcol <Michael.Coleman@sas.com>
2018-11-07 22:01:23 -0500

    added ResizeCallback to ResizeSensor.d.ts and added static functions to ResizeSensor class

--- src/ResizeSensor.d.ts
+++ src/ResizeSensor.d.ts
@@ -1,7 +1,12 @@
+export declare type ResizeCallback = (size: { width: number; height: number; }) => void;
+
 declare class ResizeSensor {
-    constructor(element: (Element | Element[]), callback: Function);
-    detach(callback: Function): void;
-    reset(element: Element | Element[]): void;
+    constructor(element: Element | Element[], callback: ResizeCallback);
+    detach(callback: ResizeCallback): void;
+    reset(): void;
+
+    static detach(element: Element | Element[], callback: ResizeCallback): void;
+    static reset(element: Element | Element[]): void;
 }
 
 export default ResizeSensor;
