1f239037237e3e866289e3b024c075069a07f445
Marc J. Schmidt <marc@marcjschmidt.de>
2019-05-09 11:47:09 +0200

    added new version

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,11 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
 var sass = require('gulp-sass');
-var markdown = require('gulp-markdown');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
-
-gulp.task('markdown', function () {
-    return gulp.src('markdown/*.md')
-        .pipe(markdown())
-        .pipe(gulp.dest('dist/'));
-});
 
 gulp.task('watch', function () {
     gulp.watch('css/*.scss', ['scss']);
     gulp.watch('app.js', ['js']);
-    gulp.watch('markdown/*.md', ['markdown']);
     gulp.watch('*.html', ['app']);
 });
 
@@ -28,26 +19,23 @@ gulp.task('scss', function () {
 });
 
 gulp.task('app', function () {
-});
-
-gulp.task('js', function () {
-    gulp.src([
-            './app.js'
-        ])
+    return gulp.src([
+        './app.js'
+    ])
         .pipe(browserify({
             insertGlobals : true
         }))
         .pipe(gulp.dest('build/js/'));
+});
 
-    gulp.src([
+gulp.task('js', function () {
+    return gulp.src([
             'node_modules/jquery/dist/jquery.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
             'build/js/app.js'
         ])
         .pipe(concat('app.js'))
         .pipe(gulp.dest('build/js/'));
-
-
 });
 
-gulp.task('default', ['app', 'scss', 'js', 'watch']);
+gulp.task('build', gulp.series(['app', 'scss', 'js']));
