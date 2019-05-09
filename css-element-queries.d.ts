abfde7967294f89ef96fa607d391744df8cbbc33
Stephen Lautier <stephen.lautier@outlook.com>
2019-05-09 11:05:02 +0200

    feat(typings): export typings

---
+++ css-element-queries.d.ts
@@ -0,0 +1,1 @@
+export { ResizeSensor, ResizeSensorCallback } from "./src/ResizeSensor";
--- src/ResizeSensor.d.ts
+++ src/ResizeSensor.d.ts
@@ -1,6 +1,6 @@
 export declare type ResizeSensorCallback = (size: { width: number; height: number; }) => void;
 
-declare class ResizeSensor {
+export declare class ResizeSensor {
     constructor(element: Element | Element[], callback: ResizeSensorCallback);
     detach(callback?: ResizeSensorCallback): void;
     reset(): void;
