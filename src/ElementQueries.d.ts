f8f713e5ea9bc003a351776bb79142bf001d3962
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-08-12 22:02:47 -0500

    Tweak comments in definition file too

--- src/ElementQueries.d.ts
+++ src/ElementQueries.d.ts
@@ -1,12 +1,12 @@
 export declare class ElementQueries {
   /**
-   * Attaches to DOMLoadContent
+   * Attaches to DOMContentLoaded
    */
   static listen(): void;
 
   /**
    * Parses all available CSS and attach ResizeSensor to those elements which have rules attached.
-   * Make sure this is called after 'load' event, because CSS files are not ready when domReady is fired.
+   * Make sure this is called after 'DOMContentLoaded' fires, because CSS files are not ready before then.
    */
   static init(): void;
 }
